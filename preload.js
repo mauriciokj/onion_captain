7271778105508312da235334c7e701d5cee7f649
Mauricio Krzesinsk Junior <mauriciokj@gmail.com>
2016-05-04 18:52:12 -0300

    new splash

--- preload.js
+++ preload.js
@@ -7,6 +7,8 @@ preload.prototype = {
         this.game.load.image('bird', 'assets/bird.png');
         this.game.load.image('pipe', 'assets/pipe.png');
 
+        this.game.load.image('background', 'assets/onion-captain-splash.png');
+
         // Load the jump sound
         this.game.load.audio('jump', 'assets/jump.wav');
 
